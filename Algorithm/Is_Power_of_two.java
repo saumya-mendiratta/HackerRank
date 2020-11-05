fb11b915fa2776de4c05693bb1b54dbf5628f7ee
saumya-mendiratta <saya.mendiratta@gmail.com>
2020-11-06 04:15:24 +0530

    Create Is_Power_of_two.java

---
+++ Algorithm/Is_Power_of_two.java
@@ -0,0 +1,19 @@
+package Algorithm;
+
+public class Is_Power_of_two {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+	}
+
+	public  static boolean ispowerof2(int a) {
+
+		return a!=0 && ((a&(a-1)) ==0)  ;
+		
+		
+    }
+
+}
