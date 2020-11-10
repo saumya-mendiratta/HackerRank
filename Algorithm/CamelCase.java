b7932351f36bb4c8f65c84ff2650b58de3dfbfd7
saumya-mendiratta <saya.mendiratta@gmail.com>
2020-11-11 00:25:17 +0530

    Update CamelCase.java

--- Algorithm/CamelCase.java
+++ Algorithm/CamelCase.java
@@ -3,8 +3,7 @@ package Algorithm;
 public class CamelCase {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 		int n= camelcase("oneTwoThree");
 		System.out.println(n);
 	}
